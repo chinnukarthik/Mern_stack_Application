e5f72e8827deb2d119c267ca7e5172d8e5ed1c9d
S Kartik <seshamkarthik3@gmail.com>
2025-06-29 19:45:00 +0530

    Theme provider change

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { Toaster } from "sonner";
 import store from "./redux/store";
-import ThemeProvider from "./components/themeProvider";
+import ThemeProvider from "./components/ThemeProvider";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
 
